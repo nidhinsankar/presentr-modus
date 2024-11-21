2925e3a37b95fb2b48771cc11ad2ed9ddd7ed47a
nidhinsankar <nidhinsankar1@gmail.com>
2024-11-21 14:22:25 +0530

    implemented function logic for invoking ai models for generating pptx content

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,7 @@
-export * from "./quote";
-export * from "./youtube";
-export * from "./todos";
+// export * from "./quote";
+// export * from "./youtube";
+// export * from "./todos";
 export * from "./presentation";
-export function sayHello(name: string | null = null): string {
-  return `Hello, ${name || "World"}!`;
-}
+// export function sayHello(name: string | null = null): string {
+//   return `Hello, ${name || "World"}!`;
+// }
