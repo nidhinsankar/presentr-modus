0b8e5b0b76859a185d5c5c49888b95a0c26afaf2
nidhinsankar <nidhinsankar1@gmail.com>
2024-11-26 23:26:32 +0530

    connect to frontend

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,1 @@
-// export * from "./quote";
-// export * from "./youtube";
-// export * from "./todos";
 export * from "./presentation";
-// export function sayHello(name: string | null = null): string {
-//   return `Hello, ${name || "World"}!`;
-// }
